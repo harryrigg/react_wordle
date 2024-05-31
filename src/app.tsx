0e849886e3e068b7b25e5fc6cf2d792daaf232af
Harry Rigg <riggh@icloud.com>
2024-05-31 16:20:24 +0800

    Add badge

--- src/app.tsx
+++ src/app.tsx
@@ -14,7 +14,19 @@ export default function Home() {
   }
 
   return (
-    <div className="h-full flex items-center justify-center">
+    <div className="relative h-full flex items-center justify-center">
+      <div className="w-max absolute bottom-0 right-1/2 flex translate-x-1/2 items-center gap-2 p-4 text-zinc-400 md:right-0 md:translate-x-0">
+        <a href="https://github.com/harryrigg/react_wordle" target="_blank">
+          <img className="h-[1.2em]" src="/github.svg" alt="github" />
+        </a>
+        •
+        <span className="w-max">
+          Built by{" "}
+          <a className="text-zinc-500" href="https://github.com/harryrigg">
+            Harry Rigg
+          </a>
+        </span>
+      </div>
       <Game key={gameKey} answer={answer} onReload={handleReload} />
     </div>
   );
